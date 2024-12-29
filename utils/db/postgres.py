9d1af92830ca1334e7b99e0fcba09b73587eca69
Asadbek Abdubannopov <abdupattoyevsardorbekjon@gmail.com>
2024-12-29 16:34:53 +0500

    Update postgres.py

--- utils/db/postgres.py
+++ utils/db/postgres.py
@@ -106,7 +106,7 @@ class Database:
         )
         return sql, tuple(parameters.values())
 
-    async def add_advertisement(self, photo_id, text, duration_minutes, created_by, group_ids):
+    async def add_advertisement(self, photo_id=None, text, duration_minutes, created_by, group_ids):
         """
         Add a new advertisement to the Advertisements table.
 
